731cfd955397014f6a4a3e178dde156241f0bca3
dalilakrslak <dkrslak1@etf.unsa.ba>
2025-03-23 19:29:32 +0100

    created project

---
+++ book-service/src/test/java/ba/unsa/etf/book/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ transfer-service/src/main/java/ba/unsa/etf/transfer_service/TransferServiceApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.transfer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferServiceApplication.class, args);
+	}
+
+}
