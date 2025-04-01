40499c6a085d4763bb8db70beef3a52c93e010d1
Dalila Kršlak <Dalila.Krslak@ziragroup.com>
2025-04-01 08:22:01 +0200

    added GenreTests

--- book-service/src/main/java/ba/unsa/etf/book/api/model/Genre.java
+++ book-service/src/main/java/ba/unsa/etf/book/api/model/Genre.java
@@ -2,11 +2,13 @@ package ba.unsa.etf.book.api.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class Genre implements Serializable {
     private static final long serialVersionUID = 1L;
 
